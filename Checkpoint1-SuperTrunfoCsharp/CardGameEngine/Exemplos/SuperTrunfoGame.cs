f62c57c0bb9577740a35567f341ac96be7bd2a83
Matheus Taylor SImon <166126462+MatheusTaylorSimomn2014@users.noreply.github.com>
2026-03-25 10:43:04 -0300

    Update SuperTrunfoGame.cs

--- Checkpoint1-SuperTrunfoCsharp/CardGameEngine/Exemplos/SuperTrunfoGame.cs
+++ Checkpoint1-SuperTrunfoCsharp/CardGameEngine/Exemplos/SuperTrunfoGame.cs
@@ -2,52 +2,55 @@ public class SuperTrunfoGame : Game<SuperTrunfoCard>
 {
     private IPlayer<SuperTrunfoCard> _currentWinner;
     private bool _gameOver;
+    private int _roundNumber;
+    private Match _currentMatch;          
+    private GameHistory _history;         
 
-    public SuperTrunfoGame(IList<IPlayer<SuperTrunfoCard>> players, IDeck<SuperTrunfoCard> deck) : base(players, deck)
+    public SuperTrunfoGame(IList<IPlayer<SuperTrunfoCard>> players, IDeck<SuperTrunfoCard> deck)
+        : base(players, deck)
     {
+        _currentMatch = new Match();
+        _currentMatch.PlayerNames.AddRange(players.Select(p => p.Name));
+        _history = new GameHistory();
     }
 
     public override void StartGame()
     {
-        Deck.Shuffle();
-
-        int cardsPerPlayer = Deck.CardsRemaining / Players.Count;
-        int remainder = Deck.CardsRemaining % Players.Count;
-        
-        for (int i = 0; i < cardsPerPlayer; i++)
-        {
-            foreach (var player in Players)
-                player.ReceiveCard(Deck.Draw());
-        }
-        
-        for (int i = 0; i < remainder; i++)
-            Deck.Draw();
 
         _gameOver = false;
         _currentWinner = null;
+        _roundNumber = 0;
+        _currentMatch.StartTime = DateTime.Now;
     }
 
     public override void NextRound()
     {
-        if (_gameOver)
-            throw new InvalidOperationException("O jogo já terminou.");
+        if (_gameOver) throw new InvalidOperationException("O jogo já terminou.");
 
         string chosenAttribute;
         if (_currentWinner == null)
-        {
-
             chosenAttribute = "População";
-        }
         else
-        {
-
             chosenAttribute = EscolherAtributo(_currentWinner);
-        }
 
         var round = new SuperTrunfoRound(chosenAttribute);
-        round.PlayRound(Players);
-
+        round.PlayRound(Players);  
+        
         _currentWinner = round.Winner;
+
+       
+        var roundRecord = new RoundRecord
+        {
+            RoundNumber = ++_roundNumber,
+            AttributeUsed = chosenAttribute,
+            Winner = round.Winner?.Name ?? "Empate"
+        };
+
+        foreach (var kvp in round.CardsPlayed)
+        {
+            roundRecord.CardsPlayed[kvp.Key.Name] = kvp.Value.Name;
+        }
+        _currentMatch.Rounds.Add(roundRecord);
 
         foreach (var player in Players)
         {
@@ -63,6 +66,8 @@ public class SuperTrunfoGame : Game<SuperTrunfoCard>
         {
             _gameOver = true;
             var winner = Players.FirstOrDefault(p => p.Hand.Count > 0);
+            _currentMatch.Winner = winner?.Name ?? "Ninguém";
+            _history.AddMatch(_currentMatch);
             Console.WriteLine(winner != null ? $"Vencedor final: {winner.Name}!" : "Empate final!");
         }
     }
